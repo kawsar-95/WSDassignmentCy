d71d8b68001eab6be827306bf4daa95a82dac423
kawsar-95 <nuruddinkawsar1995@gmail.com>
2024-10-26 03:05:45 +0600

    Add GitHub Actions workflow for Cypress tests

--- cypress/pageobjects/HomePage.js
+++ cypress/pageobjects/HomePage.js
@@ -1,29 +1,43 @@
 class HomePage {
   visit() {
     cy.visit('https://www.shohoz.com/');
+    cy.wait(5000);
   }
 
   verifyBusSelected() {
+    cy.wait(5000);
     cy.get('a[routerlink="/bus-tickets"].current').should('be.visible');
+    cy.wait(5000);
   }
 
   enterFromCity(city) {
+    cy.wait(5000);
     cy.get('#fromcity').type(city);
+    cy.wait(5000);
     cy.get('.fromcity-input > .station-filter-dropdown > :nth-child(1)').click();
+    cy.wait(5000);
   }
 
   enterToCity(city) {
+    cy.wait(5000);
     cy.get('#tocity').type(city);
+    cy.wait(5000);
     cy.get('.tocity-input > .station-filter-dropdown > :nth-child(1)').click();
+    cy.wait(5000);
   }
 
   selectDate(date) {
+    cy.wait(5000);
     cy.get('[for="doj"]').click({ force: true });
+    cy.wait(5000);
     cy.get(`:nth-child(2) > ngb-datepicker > .ngb-dp-content > .ngb-dp-month > ngb-datepicker-month > :nth-child(6) > [aria-label="${date}"] > .btn-light`).click();
+    cy.wait(5000);
   }
 
   submitSearch() {
+    cy.wait(5000);
     cy.get("button[type='submit']").click();
+    cy.wait(5000);
   }
 }
 
--- cypress/pageobjects/SearchResultsPage.js
+++ cypress/pageobjects/SearchResultsPage.js
@@ -2,32 +2,46 @@ class SearchResultsPage {
   selectBusTypeAndOperator() {
     cy.wait(10000); // Wait for the page to load
     cy.get('.mt-3 > :nth-child(2) > label').click();
+    cy.wait(5000);
     cy.get('#operator-24').check();
+    cy.wait(5000);
     cy.get('#operator-173').check();
+    cy.wait(5000);
   }
 
   selectLowestPriceTicket() {
+    cy.wait(5000);
     cy.contains('Low to High').click();
+    cy.wait(5000);
     cy.get('button.btn-book-ticket').first().click();
+    cy.wait(5000);
   }
 
   verifyAndSelectSeats() {
+    cy.wait(5000);
     cy.get('button.btn-seat.seat-available').then(seats => {
       if (seats.length < 4) {
         throw new Error('Not enough available seats to select 4 tickets.');
       }
       for (let i = 0; i < 4; i++) {
         cy.wrap(seats[i]).click();
+        cy.wait(5000);
       }
     });
   }
 
   continueBooking() {
+    cy.wait(5000);
     cy.get('.viewer-counter-msg').should('exist');
+    cy.wait(5000);
     cy.get('.stepper-continue-btn').click();
+    cy.wait(5000);
     cy.get('.boarding-points > :nth-child(1)').click();
+    cy.wait(5000);
     cy.get('.stepper-continue-btn').click();
+    cy.wait(5000);
     cy.get('.p-4').should('be.visible');
+    cy.wait(5000);
   }
 }
 
